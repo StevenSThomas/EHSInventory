80ca77b743e9b4a2702c8f576e7cd5287f5cb0f2
Jacob Yu <stonepickdiamond@gmail.com>
2025-07-03 09:58:15 -0700

    fixed bugs on product delete view

---
+++ Models/ViewModels/DeleteConfirmationView.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EHSInventory.Models;
+
+public class DeleteConfirmationView
+{
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "A comment explaining this delete is required.")]
+    public string? Comment { get; set; }
+
+    public long? CategoryId { get; set; }
+
+    public long? ProductId { get; set; }
+}
